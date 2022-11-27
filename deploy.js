d7c79bdcba5d8f1d8102f46525a03a329896accc
varManWai <laizoke98@gmail.com>
2022-11-27 15:28:11 +0800

    add the private info to env and impletement to deploy.js

--- deploy.js
+++ deploy.js
@@ -1,26 +1,27 @@
-const HDWalletProvider = require('@truffle/hdwallet-provider');
-const Web3 = require('web3');
- 
-const { abi, evm } = require('./compile');
- 
-provider = new HDWalletProvider(
-  'YOUR_MNEMONIC',
-  'YOUR_INFURA_URL'
-);
- 
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+
+const { abi, evm } = require("./compile");
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+provider = new HDWalletProvider(process.env.MNE, process.env.INFURA);
+
 const web3 = new Web3(provider);
- 
+
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
- 
-  console.log('Attempting to deploy from account', accounts[0]);
- 
+
+  console.log("Attempting to deploy from account", accounts[0]);
+
   const result = await new web3.eth.Contract(abi)
-    .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] })
-    .send({ gas: '1000000', from: accounts[0] });
- 
-  console.log('Contract deployed to', result.options.address);
+    .deploy({ data: evm.bytecode.object, arguments: ["Hi there!"] })
+    .send({ gas: "1000000", from: accounts[0] });
+
+  console.log("Contract deployed to", result.options.address);
+
   provider.engine.stop();
 };
- 
+
 deploy();
