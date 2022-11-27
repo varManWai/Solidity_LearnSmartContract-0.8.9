bf614b7bc24aaa5c67b56b8f97fb7c753044ccfa
varManWai <laizoke98@gmail.com>
2022-11-27 15:39:43 +0800

    completed

--- deploy.js
+++ deploy.js
@@ -6,20 +6,24 @@ const { abi, evm } = require("./compile");
 const dotenv = require("dotenv");
 dotenv.config();
 
-provider = new HDWalletProvider(process.env.MNE, process.env.INFURA);
+const provider = new HDWalletProvider(process.env.MNE, process.env.INFURA);
 
 const web3 = new Web3(provider);
 
 const deploy = async () => {
-  const accounts = await web3.eth.getAccounts();
+  try {
+    const accounts = await web3.eth.getAccounts();
 
-  console.log("Attempting to deploy from account", accounts[0]);
+    console.log("Attempting to deploy from account", accounts[2]);
 
-  const result = await new web3.eth.Contract(abi)
-    .deploy({ data: evm.bytecode.object, arguments: ["Hi there!"] })
-    .send({ gas: "1000000", from: accounts[0] });
+    const result = await new web3.eth.Contract(abi)
+      .deploy({ data: evm.bytecode.object, arguments: ["Hi there!"] })
+      .send({ gas: "1000000", from: accounts[2] });
 
-  console.log("Contract deployed to", result.options.address);
+    console.log("Contract deployed to", result.options.address);
+  } catch (err) {
+    console.log(err);
+  }
 
   provider.engine.stop();
 };
